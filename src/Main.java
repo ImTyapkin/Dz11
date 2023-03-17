a81581c106d9f774a739dcf81e4ebf27bf6945f1
ImTyapkin <im.tyapkin@yandex.ru>
2023-03-17 19:26:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import static java.lang.System.in;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(in);
+        System.out.println("Ввод первого числа для записи в строковую переменную: ");
+        int i1 = input.nextInt(); // вопрос 1
+        //System.out.println(new Integer(i1).toString());
+        String Odin;
+        Odin = Integer.toString(i1);
+        System.out.println(Odin);
+        System.out.println("Ввод второго числа и сохранение его в целочисленной переменной типа int: ");
+        int i2 = input.nextInt(); // вопрос 2
+        int resultInt1 = Math.max (i1, i2); //вопрос 3
+        System.out.println("Сравнили два числа и вывели большее на экран: "+ resultInt1 );
+        int resultInt2 = Math.min (i1, i2);
+        double f = resultInt2; // вопрос 4
+        System.out.println("Вывод меньшего числа, предварительно переконвертированного в тип double: " + f);
+
+    }
+}
